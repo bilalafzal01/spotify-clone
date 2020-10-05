db25c1d6a4ee87ac520f1694666d72d8e9fffcbd
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2020-10-06 00:09:24 +0500

    Login screen

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify"
+      />
+      <a href="#">LOGIN WITH SPOTIFY</a>
+      {/* Login with spotify button */}
+    </div>
+  );
+}
+
+export default Login;
