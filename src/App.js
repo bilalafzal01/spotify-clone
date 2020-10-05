3b16c39fbcd3823554329b4c17fe6be467682e68
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2020-10-06 02:51:46 +0500

    Login function implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify
+        .getMe()
+        .then((user) => {
+          console.log(user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    console.log(`Token is: `, _token);
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/Player.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Player() {
+  return <h1>Welcome to spotify</h1>;
+}
+
+export default Player;
